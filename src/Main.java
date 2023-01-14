1cd578a64dc5247b5b152e3bb4cec892d785a87b
Huda Hamdieh <huda.hamdieh.99@gmail.com>
2023-01-15 01:51:07 +0200

    my project is uploaded

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
+    private static int sequareX(int x){
+        return x*x;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(sequareX(5));
     }
 }
