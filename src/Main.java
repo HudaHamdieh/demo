77b29d44ebb173e473915b1ea1b6cfbaf35213ab
Huda Hamdieh <huda.hamdieh.99@gmail.com>
2023-01-15 03:22:07 +0200

    "cubX function is added and tested"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ public class Main {
     private static int sequareX(int x){
         return x*x;
     }
+    private static int cubeX(int x){
+        return x*x*x;
+    }
     public static void main(String[] args) {
+
         System.out.println(sequareX(5));
+        System.out.println(cubeX(5));
+
     }
 }
