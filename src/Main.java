b09fccd166ca5544cd2280a0cd35bed502bc7368
Huda mahmoud <muna.jawwad@gmail.com>
2023-01-15 04:31:47 +0200

    "degree five function is removed"

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,7 @@ public class Main {
     private static int degreeFour(int x){
         return x*x*x*x;
     }
-    private static int degreeFive(int x){
-        return x*x*x*x*x;
-    }
+
     public static void main(String[] args) {
 
         System.out.println(sequareX(5));
